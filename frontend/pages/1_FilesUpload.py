020182f36e0d9ec8ea1b04e3a261b0064924fceb
Peter <adhikarisuzal944@gmail.com>
2026-09-22 16:02:24 -0400

    file uploader complete

--- frontend/pages/1_FilesUpload.py
+++ frontend/pages/1_FilesUpload.py
@@ -5,8 +5,6 @@ import requests
 ### File uploader 
 
 uploaded_file = st.file_uploader("Please upload the files in here", type = ["pdf", "txt", "json"], accept_multiple_files=True)
-total_files = set(uploaded_file.name)
-for each in total_files:
-    st.write(total_files)
 
-st.divider()
+if uploaded_file and st.button("Submit"):
+    st.write("Successfully submitted")
