27b837f5046fd8935d251491a47fafbb8d4cc33b
Peter <adhikarisuzal944@gmail.com>
2026-09-22 16:00:42 -0400

    accepting the multiple files

--- frontend/pages/1_FilesUpload.py
+++ frontend/pages/1_FilesUpload.py
@@ -4,4 +4,9 @@ import requests
 
 ### File uploader 
 
-files = st.file_uploader("Please upload the files in here")
+uploaded_file = st.file_uploader("Please upload the files in here", type = ["pdf", "txt", "json"], accept_multiple_files=True)
+total_files = set(uploaded_file.name)
+for each in total_files:
+    st.write(total_files)
+
+st.divider()
