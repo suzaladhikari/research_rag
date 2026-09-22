408942fc4756b969d577c47065c52e3eb473e917
Peter <adhikarisuzal944@gmail.com>
2026-09-21 21:42:16 -0400

    created the form

--- frontend/app.py
+++ frontend/app.py
@@ -10,3 +10,10 @@ st.set_page_config(
 
 st.title("SentinelRAG")
 st.caption("Corporate Security made more secure")
+
+### Filling up the form 
+
+with st.form("Login Form"):
+    email = st.text_input("Work email")
+    password = st.text_input("Password", type = 'password')
+    submitted = st.form_submit_button("Sign in", use_container_width=True)
