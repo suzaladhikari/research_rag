31570b6297a0f9662f6a884c29b414ce18ace845
Peter <adhikarisuzal944@gmail.com>
2026-09-22 16:26:50 -0400

    loaded the api url successfully

--- frontend/pages/1_FilesUpload.py
+++ frontend/pages/1_FilesUpload.py
@@ -1,10 +1,15 @@
 import streamlit as st 
 import requests 
-
+import os 
+from dotenv import load_dotenv
 
 ### File uploader 
 
 uploaded_file = st.file_uploader("Please upload the files in here", type = ["pdf", "txt", "json"], accept_multiple_files=True)
 
+## API 
+load_dotenv()
+API_LINK = os.getenv("API_URL")
+print(API_LINK)
 if uploaded_file and st.button("Submit"):
     st.success("Successfully submitted")
