dbfe9035c2224daade2aaa06e08ba4503469dd0e
Peter <adhikarisuzal944@gmail.com>
2026-09-23 06:52:17 -0400

    extracting the files suffix

--- backend/app/routes/valid.py
+++ backend/app/routes/valid.py
@@ -1,5 +1,13 @@
 import json 
 from fastapi import HTTPException, UploadFile
 from pypdf import PdfReader
+from pathlib import Path
 
 ### Criterias
+MAX_FILE_SIZE = 10 * 1024 * 1024 ## 10 MB 
+ALLOWED_EXTENSIONS = {".pdf", ".txt", ".json"}
+
+async def validate_file(file:UploadFile)->bytes:
+    extension = Path(file.filename or "").suffix.lower() ## Getting the type of file in the lower format to check if the file matches the criteria 
+
+    
